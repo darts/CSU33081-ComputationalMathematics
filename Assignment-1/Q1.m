d3892f365c4290a2b97010218632792507a942f5
darts <darts@tcd.ie>
2020-02-21 14:05:30 +0000

    Q1 done

--- Assignment-1/Q1.m
+++ Assignment-1/Q1.m
@@ -8,8 +8,34 @@ function D = Determinant (A)
         D = "The matrix must be square.";
         return
     end
-    D= det(A);
-    if(D < 0.000000000001 && D > -0.000000000001) %ignore floating point error
-       D = 0;   
+    D = detRecurse(A);
+end
+
+function detSum = detRecurse(mat)
+    detSum = 0;
+    matSize = size(mat);
+    if matSize(1,1) ~= 2
+        pos = true;
+        for i = 1:matSize(1,1)
+            tmpSum = mat(1,i)*detRecurse(getSubMat(mat, 1, i));
+            if pos
+                detSum = detSum + tmpSum;
+            else
+                detSum = detSum - tmpSum;
+            end
+            pos = ~pos;
+        end
+    else
+        detSum = (mat(1,1)*mat(2,2))-(mat(2,1)*mat(1,2));
     end
 end
+
+function subMat = getSubMat(mat, row, col)    
+    matSize = size(mat);
+    rowVec = 1:matSize(1,1);
+    colVec = 1:matSize(1,2);
+    rowVec(row) = [];
+    colVec(col) = [];
+    subMat = mat(rowVec, colVec);
+end
+
