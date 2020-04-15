f1953470a6cf07481c7b273ba1d7128e15cbeb58
darts <darts@tcd.ie>
2020-04-15 18:34:12 +0100

    Update Q1.m

--- Assignment-3/Q1.m
+++ Assignment-3/Q1.m
@@ -2,5 +2,5 @@ x = InfinityNorm([-2 1 0; 1 -2 1; 0 1 -1.5])
 y = InfinityNorm([4 -1 0 1 0; -1 4 -1 0 1; 0 -1 4 -1 0; 1 0 -1 4 -1; 0 1 0 1 -4])
 
 function N = InfinityNorm (A)
-    N = norm(A, inf);
+    N = max(sum(abs(A)));
 end
